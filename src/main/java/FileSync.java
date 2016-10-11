94f23e3816af55aa930f0dd0ca4b83c45b7a3cd3
Ivan <ivshapovalov@gmail.com>
2016-10-11 11:45:35 +0300

    after ref

--- src/main/java/FileSync.java
+++ src/main/java/FileSync.java
@@ -5,15 +5,44 @@ import java.util.Calendar;
 import java.util.List;
 
 public class FileSync {
+    private Path srcPath;
+    private Path destPath;
+
+    public FileSync(Path srcPath,Path destPath) {
+        this.destPath = destPath;
+        this.srcPath = srcPath;
+    }
+
     public static void main(String[] args) {
+
         if (IllegalParameters(args)) {
             return;
         }
 
-        System.out.println("Синхронизация начата: " + Calendar.getInstance().getTime());
-
         Path srcPath = FileSystems.getDefault().getPath(args[0]);
         Path destPath = FileSystems.getDefault().getPath(args[1]);
+
+        new FileSync(srcPath, destPath).synchronize();
+    }
+
+    private static boolean IllegalParameters(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Количество параметров должно быть равно двум. Source каталог и Destination каталог");
+            return true;
+        }
+        String srcDir = args[0];
+        Path srcPath = FileSystems.getDefault().getPath(srcDir);
+        if (!Files.exists(srcPath)) {
+            System.out.println("Errors: Source directory does not exists!. Exit program");
+            return true;
+        }
+        return false;
+    }
+
+
+    void synchronize() {
+        System.out.println("Синхронизация начата: " + Calendar.getInstance().getTime());
+
         if (!Files.exists(destPath)) {
             try {
                 Files.createDirectory(destPath);
@@ -23,26 +52,12 @@ public class FileSync {
                 e.printStackTrace();
             }
         }
-        updateNewFiles(srcPath, destPath);
-        deleteRemovedFiles(srcPath, destPath);
+        updateNewFiles();
+        deleteRemovedFiles();
         System.out.println("Синхронизация окончена: " + Calendar.getInstance().getTime());
     }
 
-    private static boolean IllegalParameters(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Количество параметров должно быть равно двум. Source каталог и Destination каталог");
-        }
-        String srcDir = args[0];
-        String destDir = args[1];
-        Path srcPath = FileSystems.getDefault().getPath(srcDir);
-        if (!Files.exists(srcPath)) {
-            System.out.println("Errors: Source directory does not exists!. Exit program");
-            return true;
-        }
-        return false;
-    }
-
-    private static void updateNewFiles(Path srcPath, Path destPath) {
+    private void updateNewFiles() {
         int srcPathCount = srcPath.getNameCount();
         List<Path> sourceFiles = new ArrayList<>();
         try {
@@ -85,7 +100,7 @@ public class FileSync {
         }
     }
 
-    private static void deleteRemovedFiles(Path srcPath, Path destPath) {
+    private void deleteRemovedFiles() {
         int destPathCount = destPath.getNameCount();
         List<Path> sourceFiles = new ArrayList<>();
         try {
@@ -110,7 +125,7 @@ public class FileSync {
         }
     }
 
-    private static List<Path> listSourceFiles(Path dir) throws IOException {
+    private List<Path> listSourceFiles(Path dir) throws IOException {
         List<Path> result = new ArrayList<>();
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
             for (Path entry : stream) {
@@ -125,3 +140,4 @@ public class FileSync {
         return result;
     }
 }
+
